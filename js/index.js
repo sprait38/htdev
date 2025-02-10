51661ca2a762b6d59521f959eebfec79b38b6f2b
sprait38 <122175964+sprait38@users.noreply.github.com>
2025-02-11 00:02:13 +0500

    dom

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
-
- 
+var swiper1 = new Swiper('.mySwiper', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    freeMode: true,
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination1",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+      },
+  });
+  var swiper2 = new Swiper('.Duffeer_swiper', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    freeMode: true,
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination2",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
