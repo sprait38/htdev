e1bf87cb66b840208fc4170bab35573c36e4f416
sprait38 <122175964+sprait38@users.noreply.github.com>
2025-02-14 23:00:13 +0500

    finisch

--- js/index.js
+++ js/index.js
@@ -6,11 +6,7 @@ var swiper1 = new Swiper('.mySwiper', {
         },
         1019: {
             slidesPerView: 2,
-            spaceBetween: 25,
-            allowTouchMove: true,
-            allowSlideNext: true,
-            allowSlidePrev: true,
-            enabled: true,
+            spaceBetween: 0,
         },
         1339: {
             slidesPerView: 0,
@@ -43,7 +39,7 @@ var swiper2 = new Swiper('.Duffeer_swiper', {
         },
         1019: {
             slidesPerView: 2,
-            spaceBetween: 0,
+            spaceBetween: 26,
             allowTouchMove: true,
             allowSlideNext: true,
             allowSlidePrev: true,
@@ -74,44 +70,33 @@ var swiper2 = new Swiper('.Duffeer_swiper', {
 
 
 !function (e) {
-    "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector || 
+    "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector ||
         function (e) { for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t;)++n; return Boolean(o[n]) }),
-        "function" != typeof e.closest && (e.closest = function (e) { for (var t = this; t && 1 === t.nodeType;) { if (t.matches(e)) return t; t = t.parentNode } 
-        return null })
-    }(window.Element.prototype);
+        "function" != typeof e.closest && (e.closest = function (e) {
+            for (var t = this; t && 1 === t.nodeType;) { if (t.matches(e)) return t; t = t.parentNode }
+            return null
+        })
+}(window.Element.prototype);
 
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    /* Записываем в переменные массив элементов-кнопок и подложку.
-       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
     var modalButtons = document.querySelectorAll('.js-open-modal'),
         overlay = document.querySelector('.js-overlay-modal'),
         closeButtons = document.querySelectorAll('.js-modal-close');
 
-    /* Перебираем массив кнопок */
     modalButtons.forEach(function (item) {
 
-        /* Назначаем каждой кнопке обработчик клика */
         item.addEventListener('click', function (e) {
 
-            /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-               люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-               Нужно подстраховаться. */
             e.preventDefault();
-
-            /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-               и будем искать модальное окно с таким же атрибутом. */
             var modalId = this.getAttribute('data-modal'),
                 modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-            /* После того как нашли нужное модальное окно, добавим классы
-               подложке и окну чтобы показать их. */
             modalElem.classList.add('active');
             overlay.classList.add('active');
-        }); // end click
+        });
 
-    }); // end foreach
+    });
 
     closeButtons.forEach(function (item) {
 
@@ -122,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
             overlay.classList.remove('active');
         });
 
-    }); // end foreach
+    });
 
 
     document.body.addEventListener('keyup', function (e) {
@@ -141,4 +126,4 @@ document.addEventListener('DOMContentLoaded', function () {
         this.classList.remove('active');
     });
 
-}); // end ready
+});
